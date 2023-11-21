c6acd7831f44eea88ca4b314495e179058ef99b6
Michael Dasaro <michaelgdasaro@gmail.com>
2023-11-21 14:18:48 -0500

    test code and stl progress

--- RaspberryPiCode/IGOR.py
+++ RaspberryPiCode/IGOR.py
@@ -9,9 +9,20 @@ import numpy as np
 import signal
 import math
 import io
+from picamera import PiCamera
 
 
 ser = serial.Serial('/dev/ttyACM1', 115200)
 ser.write(b'G28 X0\r\n')
 
 ser.close()
+
+
+def camera_test():
+    camera = PiCamera()
+    camera.capture('/home/pi/Desktop/image.jpg')
+
+
+# raspistill -o Desktop/image.jpg
+# libcamera-still -o test.jpg
+# scp pi@raspberrypi:test.jpg .
